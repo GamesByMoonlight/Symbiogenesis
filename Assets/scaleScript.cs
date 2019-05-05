74bf1b5767a8492f74be23df07791ba106aba17c
ciscom <ciscom1971@yahoo.com>
2019-05-05 12:19:02 -0500

    Added scene

---
+++ Assets/scaleScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scaleScript : MonoBehaviour
+{
+   
+    Transform trans;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trans = GetComponent<Transform>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (trans.localScale.x > collision.collider.transform.localScale.x)
+        {
+            trans.localScale += collision.collider.transform.localScale;
+
+        }
+
+        trans.localScale += collision.collider.transform.localScale;
+
+    }
+
+}
