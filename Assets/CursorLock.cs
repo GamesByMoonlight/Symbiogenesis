7d033d4867026b39cc055b656a70d54d86a05a93
ciscom <ciscom1971@yahoo.com>
2019-05-28 23:12:31 -0500

    Added, player name and player Size, added cursor unlock.

---
+++ Assets/CursorLock.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorLock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Escape) )
+        {
+            CursorUnlock();
+        }
+
+        if (Input.GetKey(KeyCode.X))
+        {
+            CursorUnlock();
+
+        }
+
+        if(Input.GetKey(KeyCode.L))
+        {
+            CursorUnlockFalse();
+        }
+        if (Input.GetKey(KeyCode.U))
+        {
+            CursorUnlock();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            CursorUnlock();
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            CursorUnlock();
+        }
+        if (Input.GetMouseButtonDown(2))
+        {
+            CursorUnlock();
+        }
+
+
+    }
+
+    public void CursorUnlockFalse()
+    {
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+
+
+
+    public void CursorUnlock()
+    {
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/SetUpLocalPlayer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetUpLocalPlayer : NetworkBehaviour
+{
+    [SyncVar]
+    public string pname = "Player";
+
+    public void OnGUI()
+    {
+       if(isLocalPlayer)
+        {
+            pname = GUI.TextField(new Rect(25, Screen.height - 40, 100, 30), pname);
+            if(GUI.Button(new Rect( 130, Screen.height - 40, 80,30),"Change"))
+            {
+                CmdChangeName(pname );
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(isLocalPlayer)
+        {
+//            GetComponent<drive>().enabled = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isLocalPlayer)
+        {
+            float scaleSize = this.transform.localScale.x * 100;
+            int scaleSizeInt = (int)scaleSize;
+            this.GetComponentInChildren<TextMesh>().text = $"{pname} {scaleSizeInt.ToString()} ";
+
+        }
+        else
+        {
+            this.GetComponentInChildren<TextMesh>().text = $"{pname}";
+        }
+
+
+
+        CmdChangeName(pname);
+
+    }
+
+
+    [Command]
+    public void CmdChangeName(string newName)
+    {
+        pname = newName;
+    }
+
+}
